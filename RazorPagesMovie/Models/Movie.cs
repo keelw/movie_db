7df43cb2b48779593a5663207e143da5d596c597
keelw <wjordankeel@gmail.com>
2024-06-22 17:46:58 -0600

    work in progress

---
+++ RazorPagesMovie/Models/Movie.cs
@@ -0,0 +1,16 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RazorPagesMovie.Models;
+
+public class Movie
+{
+    public int Id { get; set; }
+    public string? Title { get; set; }
+    [Display(Name = "Release Date")]
+    [DataType(DataType.Date)]
+    public DateTime ReleaseDate { get; set; }
+    public string? Genre { get; set; }
+    [Column(TypeName = "decomal(18, 2)")]
+    public decimal Price { get; set; }
+}
